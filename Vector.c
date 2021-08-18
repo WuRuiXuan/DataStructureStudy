a59d539347e3ea80ce93d75c9fa0d1ab907e00e0
巫瑞轩 <wuruixuan@wuruixuandeMacBook-Pro.local>
2021-08-18 22:30:55 +0800

    Vector

--- Vector.c
+++ Vector.c
@@ -1,6 +1,6 @@
 //
 //  顺序表
-//  CStudy
+//  Vector
 //
 //  Created by 巫瑞轩 on 2021/8/17.
 //
@@ -8,6 +8,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+#define COLOR(a, b) "\033[" #b "m" a "\033[0m"
+#define GREEN(a) COLOR(a, 32)
 
 // 结构定义
 typedef struct Vector {
@@ -18,6 +21,7 @@ typedef struct Vector {
 // 结构操作
 Vector *m_init(int);
 void m_clear(Vector *);
+int m_expand(Vector *);
 int m_insert(Vector *, int, int);
 int m_erase(Vector *, int);
 void m_output(Vector *);
@@ -25,19 +29,23 @@ void m_output(Vector *);
 int main() {
     // 使用随机函数必须先初始化随机数发生器(需要引入time.h)
     srand(time(0));
+    // 随机执行操作的次数
     #define MAX_OP 20
-    Vector *v = m_init(MAX_OP);
+    Vector *v = m_init(1);
     for (int i = 0; i < MAX_OP; i++) {
-        int op = rand() % 2;
+        int op = rand() % 4;
         int val = rand() % 100;
+        // -1 ~ length+1
         int idx = rand() % (v->length + 3) - 1;
         switch (op) {
             case 0:
+            case 1:
+            case 2:
             {
                 printf("insert %d at %d to Vector = %d\n", val, idx, m_insert(v, idx, val));
             }
                 break;
-            case 1:
+            case 3:
             {
                 printf("erase a item at %d from Vector = %d\n", idx, m_erase(v, idx));
             }
@@ -45,6 +53,7 @@ int main() {
                 break;
         }
         m_output(v);
+        printf("\n");
     }
     return 0;
 }
@@ -69,10 +78,28 @@ void m_clear(Vector *v) {
     return;
 }
 
+// 扩容
+int m_expand(Vector *v) {
+    int extr_size = v->size;
+    int *p;
+    while (extr_size) {
+        p = (int *)realloc(v->data, sizeof(int) * (v->size + extr_size));
+        if (p != NULL) break;
+        extr_size >>= 1;
+    }
+    if (p == NULL) return 0;
+    v->data = p;
+    v->size += extr_size;
+    return 1;
+}
+
 // 插入
 int m_insert(Vector *v, int idx, int val) {
     if (v == NULL) return 0;
-    if (v->length == v->size) return 0;
+    if (v->length == v->size) {
+        if (!m_expand(v)) return 0;
+        printf(GREEN("success to expand! the size = %d\n"), v->size);
+    }
     if (idx < 0 || idx > v->length) return 0;
     // 从末尾开始依次往前挪，反之则会出现后面挪动的值被第一个挪动的值所覆盖的情况
     for (int i = v->length; i > idx; i--) {
@@ -86,9 +113,10 @@ int m_insert(Vector *v, int idx, int val) {
 // 删除
 int m_erase(Vector *v, int idx) {
     if (v == NULL) return 0;
-    if (idx < 0 || idx > v->length) return 0;
-    for (int i = idx; i < v->length; i++) {
-        v->data[i] = v->data[i+1];
+    if (idx < 0 || idx >= v->length) return 0;
+    // 从删除位置开始，依次往后用后面的值覆盖前面的值
+    for (int i = idx; i < v->length - 1; i++) {
+        v->data[i] = v->data[i + 1];
     }
     v->length -= 1;
     return 1;
